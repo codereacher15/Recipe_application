eb6f49c64043d438573d9e51a23012970b2bb314
codereacher15 <mayukh1503.2002@gmail.com>
2023-03-02 22:48:34 +0530

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -101,12 +101,7 @@ class _MyAppState extends State<MyApp> {
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
-        // if (settings.name == '/meal-detail') {
-        //   return ...;
-        // } else if (settings.name == '/something-else') {
-        //   return ...;
-        // }
-        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+        
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
